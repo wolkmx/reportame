b5f404bbbd6841f8e60a4ae7bcefd261f980c93b
Roberto Hernández De La Luz <rhernandez@netosfera.com>
2014-10-22 11:10:54 -0430

    Se agrego Jquery a la carpeta ui para manejar los eventos, se agregaron diversos templates para el formulario de registro y se agrego la logica en ajax para identificar si el usuario es nuevo, se esta registrando o no

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ $f3->set('ENCODING','UTF8');
 //Se declara la conexion a la base de datos
 $db=new DB\SQL(
     'mysql:host=localhost;port=3306;dbname=reportame',
-    'root',
-    'sqlnetos'
+    'reportame',
+    'reportame'
 );
 
 //Admin Add
@@ -43,6 +43,59 @@ $f3->route('GET /',
 	
 	
     }
+);
+
+/*Ruta para recibir la peticion ajax para mostrar el formulario de registro*/
+$f3->route('POST /login [ajax]',
+	function($f3) {
+		echo Template::instance()->render('loginform.html');
+	}
+);
+
+/*Ruta para recibir la peticion ajax para iniciar sesion*/
+/*el valor extra [ajax] se utiliza para indicar que es una peticion ajax, si no es una peticion ajax no entrara a esta url, es decir si se coloca iniciarsesion en el navegador no entrara*/
+/*Se utiliza use ($db) para que la funcion pueda acceder a la conexion de base de datos definida previamente*/
+$f3->route('POST /iniciarsesion [ajax]',
+	function($f3) use ($db){
+		//Se obtienen los datos enviados por el formulario
+		$formulario = $f3->get("REQUEST");
+		
+		/*Se consulta la base de datos para ver si existe un usuario con este email y clave*/
+		$usuario = $db->exec('SELECT * FROM usuario WHERE alias LIKE "'.$formulario['user'].'" OR email LIKE "'.$formulario['user'].'" AND password="'.$formulario['clave'].'"');
+		
+		$f3->set('flash',Null);
+		
+		if(count($usuario)){
+			//Si encuentra el usuario lo deberia redireccionar a su panel de control.
+			echo "encontrado";
+		}else{
+		//Si no encuentra el usuario volvera a mostrar el formulario con un mensaje de advertencia llamado flash
+			$f3->set('flash','Usuario o clave incorrecta por favor ingrese los datos nuevamente');
+			echo Template::instance()->render('loginform.html');
+
+		}
+		
+		//Se obtiene el  mapeo del objeto de tipo usuario desde la base de datos
+		/*$user=new DB\SQL\Mapper($db,'usuario');
+		$user->load(array('usuario=?','tarzan'));*/
+		
+		
+		/*echo "<pre>";
+		print_r($formulario);
+		echo "</pre>";*/
+		//echo Template::instance()->render('loginform.html');
+	}
+);
+
+/*Ruta para recibir la peticion ajax para registrar el usuario*/
+$f3->route('POST /registrousuario [ajax]',
+	function($f3) {
+		$formulario = $f3->get("REQUEST");
+		echo "<pre>";
+		print_r($formulario);
+		echo "</pre>";
+		//echo Template::instance()->render('loginform.html');
+	}
 );
 
 /*
