4db3fc269b8810876e9f278d17f4f3005b5a3569
Roberto Hernández De La Luz <rhernandez@netosfera.com>
2014-10-23 12:07:00 -0430

    Se termino la funcionalidad para iniciar sesion y registrar un nuevo usuario, falta aun agregar varias verificaciones para saber si el usuario ya existe o no en la base de datos cuando intenta registrarse.

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ $f3->config('config.ini');
 
 //Codificacion
 $f3->set('ENCODING','UTF8');
+//Se inicializa el cache
+$f3->set('CACHE','memcache=localhost');
 
 //Se declara la conexion a la base de datos
 $db=new DB\SQL(
@@ -25,16 +27,29 @@ $f3->route('GET /user/new',
 );
 
 //Home page
-$f3->route('GET /',
+$f3->route('GET @home: /',
     function($f3) use  ($db) {
 	//Se indica que el contenido del template lo tomara de home.html
 	$f3->set('content','home.html');
 	
+	/*@todo crear if inicio sesion para que vaya a su panel*/
+	
 	//Se realiza una consulta a la base de datos
 	//$f3->set('result',$db->exec('SELECT alias FROM usuario'));
 	
-	$user=new DB\SQL\Mapper($db,'usuario');
+	$user=new DB\SQL\Mapper($db,'Usuario');
 	$user->load(array('alias=?','wolkmx'));
+	
+	/*Se obtiene el arreglo de la sesion para saber si existe el key user*/
+	$sesion = $f3->get('SESSION');
+	/*Si no existe se declara nula*/
+	if(!array_key_exists("user",$sesion)){
+		$f3->set('usuario','');
+	}
+	
+	/*echo "---<pre>";
+	print_r($sesion);
+	echo "</pre>---";*/
 	
 	$f3->set('nombre',$user);
 	
@@ -46,8 +61,11 @@ $f3->route('GET /',
 );
 
 /*Ruta para recibir la peticion ajax para mostrar el formulario de registro*/
-$f3->route('POST /login [ajax]',
+$f3->route('POST @login: /login [ajax]',
 	function($f3) {
+	
+		//Se declara la variable flash como nula para que no aparesca la primera vez que se muestra el formulario pero si la segunda vez si es que hay un error.
+		$f3->set('flash',null);
 		echo Template::instance()->render('loginform.html');
 	}
 );
@@ -55,19 +73,33 @@ $f3->route('POST /login [ajax]',
 /*Ruta para recibir la peticion ajax para iniciar sesion*/
 /*el valor extra [ajax] se utiliza para indicar que es una peticion ajax, si no es una peticion ajax no entrara a esta url, es decir si se coloca iniciarsesion en el navegador no entrara*/
 /*Se utiliza use ($db) para que la funcion pueda acceder a la conexion de base de datos definida previamente*/
-$f3->route('POST /iniciarsesion [ajax]',
+$f3->route('POST @iniciarsesion: /iniciarsesion [ajax]',
 	function($f3) use ($db){
 		//Se obtienen los datos enviados por el formulario
 		$formulario = $f3->get("REQUEST");
 		
+		/*echo "<pre>";
+		print_r($formulario);
+		echo "</pre>";*/
+		
 		/*Se consulta la base de datos para ver si existe un usuario con este email y clave*/
-		$usuario = $db->exec('SELECT * FROM usuario WHERE alias LIKE "'.$formulario['user'].'" OR email LIKE "'.$formulario['user'].'" AND password="'.$formulario['clave'].'"');
+		$usuario = $db->exec('SELECT * FROM Usuario WHERE (alias LIKE "'.$formulario['user'].'" OR email LIKE "'.$formulario['user'].'") AND password="'.$formulario['clave'].'"');
 		
 		$f3->set('flash',Null);
 		
 		if(count($usuario)){
-			//Si encuentra el usuario lo deberia redireccionar a su panel de control.
-			echo "encontrado";
+			//Si encuentra el usuario se inicia la sesion con los datos del usuario.
+			new Session();
+			
+			$f3->set('SESSION.user',$usuario[0]['alias']);
+			$f3->set('SESSION.id',$usuario[0]['idUsuario']);
+			
+			//echo "--".$f3->get('SESSION.user')."--";
+			
+			//Se redirige al usuario a la ruta de panel de control
+			//$f3->reroute('@miPanel'); 
+			echo '<script language="javascript" type="text/javascript">window.location.href="mipanel/";</script>';
+			
 		}else{
 		//Si no encuentra el usuario volvera a mostrar el formulario con un mensaje de advertencia llamado flash
 			$f3->set('flash','Usuario o clave incorrecta por favor ingrese los datos nuevamente');
@@ -88,13 +120,68 @@ $f3->route('POST /iniciarsesion [ajax]',
 );
 
 /*Ruta para recibir la peticion ajax para registrar el usuario*/
-$f3->route('POST /registrousuario [ajax]',
-	function($f3) {
+$f3->route('POST @registrousuario: /registrousuario [ajax]',
+	function($f3) use ($db) {
 		$formulario = $f3->get("REQUEST");
-		echo "<pre>";
+		
+		/*echo "<pre>";
 		print_r($formulario);
 		echo "</pre>";
-		//echo Template::instance()->render('loginform.html');
+		die();*/
+		$user=new DB\SQL\Mapper($db,'Usuario');
+		$user->set('alias',$formulario['user']);
+		$user->set('email',$formulario['email']);
+		$user->set('password',$formulario['clave']);
+		//Se guarda el usuario creado
+		$user->save();
+		
+		/*@todo falta agregar la logica para verificar si ya existe le usuario o si se pudo guardar correctamente por ejemplo que no exista el usuario y el email antes*/
+		
+		//Si encuentra el usuario se inicia la sesion con los datos del usuario.
+			new Session();
+			
+			$f3->set('SESSION.user',$user->get('alias'));
+			$f3->set('SESSION.id',$user->get('idUsuario'));
+
+		//Se redirecciona al panel de control
+		echo '<script language="javascript" type="text/javascript">window.location.href="mipanel/";</script>'; 
+	}
+);
+
+/*Ruta para mostrar al usuario su panel de control*/
+$f3->route('GET  @miPanel: /mipanel',
+	function($f3) {
+	/*Se debe volver a instanciar el objeto de tipo sesion para poder acceder a los datos globales si no no funcionara!!!*/
+	new Session();
+	//echo "--".$f3->get('SESSION.user')."--";
+		/*Se verifica si el usuario tiene la sesion iniciada*/
+		if( ('' !== $f3->get('SESSION.user')) && (NULL !== $f3->get('SESSION.user'))){
+			$f3->set('content','panel/paneldecontrol.html');
+			$f3->set('menu','menu.html');
+			$f3->set('usuario',$f3->get('SESSION.user'));
+			
+			echo Template::instance()->render('layout.html');
+		}else{
+			/*Si no se reenvia al home*/
+			$f3->reroute('@home'); 
+		}
+		
+	}
+);
+
+/*Ruta para cerrar la sesion*/
+$f3->route('GET  @logout: /logout',
+	function($f3) {
+	
+		//Se inicializa la sesion para poder acceder a los valores anteriores
+		new Session();
+		//Se destruye la sesion del usuario
+		$f3->clear('SESSION.user');
+		$f3->clear('SESSION.id');
+		$f3->clear('SESSION');
+		//session_destroy();
+		$f3->reroute('@home'); 
+		//echo Template::instance()->render('layout.html');
 	}
 );
 
