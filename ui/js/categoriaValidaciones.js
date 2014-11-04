0eeee2542f161807cc2d811d7f3d49c87184f04a
Oscar Galindez <ogalindez@netosfera.com>
2014-11-04 11:20:50 -0430

    Se agregan las funcionalidades para los modulos 'perfil', 'enfermedad' y 'categoria'

---
+++ ui/js/categoriaValidaciones.js
@@ -0,0 +1,17 @@
+//Funcion para capturar el avance del formulario de reporte de incidencia
+$(".validarCategoria").click(function(event){
+    
+    var valido = true;
+    var campos = '';
+    
+    if( $("#nombre").val() == "" ){
+        valido = false;
+        campos += '\nNombre [Obligatorio]';
+    }
+    
+    if( !valido ){
+            alert("CAMPO:\n" + campos);
+            return false;
+    }
+    
+});
